f5b943630771e01cd97272502f38cbc99675480c
YigitKDA <154975871+YigitKDA@users.noreply.github.com>
2026-05-26 14:23:44 +0300

    Create weighted_yigit_kizildag.py

---
+++ weighted_yigit_kizildag.py
@@ -0,0 +1,7 @@
+import random
+
+def weighted_srs(data: list,n: int,weights: list,with_replacement: bool= False):
+    if(with_replacement):
+        return random.choices(population=data,weights=weights,cum_weights=None,k=n)
+      
+    return random.sample(population=data,k=n,counts=weights)
